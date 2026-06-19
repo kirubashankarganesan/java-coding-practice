bf388841fb854c4994e439b6f9f67f81b8df8da0
kirubashankarganesan <kirubashankarg2005@gmail.com>
2026-06-19 06:23:08 +0530

    first commit

---
+++ pattern/star/star3.java
@@ -0,0 +1,19 @@
+package pattern.star;
+
+public class star3 {
+     public static void main(String[] args) {
+        int n=5;
+         for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
