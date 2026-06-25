32f3047c403e1ed1308f3cf5f1dd6a6759bdf188
kirubashankarganesan <kirubashankarg2005@gmail.com>
2026-06-25 17:13:03 +0530

    first commit

--- pattern/string/s1.java
+++ pattern/string/s1.java
@@ -1,5 +1,20 @@
 package pattern.string;
 
 public class s1 {
-    
+    public static void main(String[] args) {
+        String s = "CODER";
+        int n = s.length();
+        for (int i = 1, p = 0; i <= n; i++, p++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(s.charAt(p) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+// C 
+// O O 
+// D D D 
+// E E E E 
+// R R R R R 
