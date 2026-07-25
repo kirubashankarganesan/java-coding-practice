78f9d4142aa54d0f9d96b105051a75b4d92b664b
kirubashankarganesan <kirubashankarg2005@gmail.com>
2026-07-25 09:39:50 +0530

    first commit

---
+++ array_2d/upper_diagonal_minor_included.java
@@ -0,0 +1,18 @@
+package array_2d;
+
+public class upper_diagonal_minor_included {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int n = arr.length;
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i+j<=n-1) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// 1 2 3 4 5 7
