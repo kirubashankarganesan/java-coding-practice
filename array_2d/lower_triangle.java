95eabbfc2b0c7dc19aef5d6e71552a13d58b7f27
kirubashankarganesan <kirubashankarg2005@gmail.com>
2026-07-25 09:35:54 +0530

    first commit

---
+++ array_2d/lower_triangle.java
@@ -0,0 +1,16 @@
+package array_2d;
+
+public class lower_triangle {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int n = arr.length;
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i>=j) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
